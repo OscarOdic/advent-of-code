13fed3b7e56235cb71dd7b94cb5279c8e337e0cf
Oscar Odic <odic.oscar@gmail.com>
2017-12-05 14:16:48 +0100

    Day1 + Day2

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := """advent_of_code_2017"""
+
+version := "1.0"
+
+scalaVersion := "2.12.4"
+
---
+++ src/main/scala/adventOfCode/Day1.scala
@@ -0,0 +1,24 @@
+package adventOfCode
+
+import scala.io.Source
+
+object Day1 {
+  val input = Source.fromResource("day1.txt").getLines().mkString("\n")
+
+  def resolveWithN(list: Seq[Int], n: Int): Int = list.zipWithIndex.map {
+    case (value, index) if index+n < list.size => (value, list(index+n))
+    case (value, index) => (value, list(index + n - list.size))
+  }.foldLeft(0) {
+    case (acc, (left, right)) if left == right => acc + left
+    case (acc, _) => acc
+  }
+
+  def resolveFirst(list: Seq[Int]): Int = resolveWithN(list, 1)
+
+  def resolveSecond(list: Seq[Int]): Int = resolveWithN(list, list.size/2)
+
+  def main(args: Array[String]): Unit = {
+    println(s"[first star] ${resolveFirst(input.map(_.asDigit))}")
+    println(s"[second star] ${resolveSecond(input.map(_.asDigit))}")
+  }
+}
