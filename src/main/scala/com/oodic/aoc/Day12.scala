e72c4216651e12e4a2cab47976d45e464310667f
Oscar Odic <odic.oscar@gmail.com>
2017-12-12 15:12:05 +0100

    Add travis

--- src/main/scala/com/oodic/aoc/Day12.scala
+++ src/main/scala/com/oodic/aoc/Day12.scala
@@ -6,18 +6,17 @@ import scala.util.parsing.combinator.RegexParsers
 object Day12 extends RegexParsers {
   val input = Source.fromResource("day12.txt").getLines.toList
 
-  val communicationParser: Parser[Map[Int, List[Int]]] = for {
+  val communicationParser: Parser[Map[Int, Set[Int]]] = for {
     program <- """\d+""".r ^^ (_.toInt)
     _ <- """<->""".r
     programs <- rep1sep("""\d+""".r ^^ (_.toInt), ", ")
-  } yield Map(program -> programs)
+  } yield Map(program -> programs.toSet)
 
-  def getGroup(origin: Int, communications: Map[Int, List[Int]]) = {
-    def rec(contains: List[Int] = List(origin), communicate: List[Int] = List(origin)): List[Int] = {
-      communicate.flatMap(communications).distinct.diff(contains) match {
-        case Nil => contains
-        case l => rec(contains.union(l), l)
-      }
+  def getGroup(origin: Int, communications: Map[Int, Set[Int]]) = {
+    def rec(contains: Set[Int] = Set(origin), communicate: Set[Int] = Set(origin)): Set[Int] = {
+      val next = communicate.flatMap(communications).diff(contains)
+      if (next.isEmpty) contains
+      else rec(contains.union(next), next)
     }
     rec()
   }
@@ -31,7 +30,7 @@ object Day12 extends RegexParsers {
     val communications = input.map(parse(communicationParser, _).get).reduce(_ ++ _)
     def rec(keys: List[Int] = communications.keys.toList, n: Int = 0): Int = keys match {
       case Nil => n
-      case h::t => rec(keys.diff(getGroup(h, communications)), n+1)
+      case h::_ => rec(keys.diff(getGroup(h, communications).toList), n+1)
     }
     rec()
   }
