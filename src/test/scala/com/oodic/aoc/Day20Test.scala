5acb1e91e0bf721522f45e2682381e48893b8003
Oscar Odic <odic.oscar@gmail.com>
2017-12-20 11:21:55 +0100

    Day20

---
+++ src/test/scala/com/oodic/aoc/Day20Test.scala
@@ -0,0 +1,29 @@
+package com.oodic.aoc
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Day20Test extends FlatSpec with Matchers {
+  it should "answer first part" in {
+    val testInput = List(
+      "p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>",
+      "p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>"
+    )
+
+    Day20.resolveFirst(testInput) should be(0)
+
+    Day20.resolveFirst(Day20.input) should be(157)
+  }
+
+  it should "answer second part" in {
+    val testInput = List(
+      "p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>",
+      "p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>",
+      "p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>",
+      "p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>"
+    )
+
+    Day20.resolveSecond(testInput) should be(1)
+
+    Day20.resolveSecond(Day20.input) should be(499)
+  }
+}
