69f8e4f8d860ac52b6d9855f74b4a1337bbf2e95
Oscar Odic <odic.oscar@gmail.com>
2022-12-06 10:54:22 +0100

    Day06 - 2022

---
+++ src/main/scala/com/oodic/aoc/y2022/Day06.scala
@@ -0,0 +1,23 @@
+package com.oodic.aoc.y2022
+
+object Day06 extends Puzzle2022[String, Int, Int] {
+  override val input: String = getInputFile.head
+
+  private def allDifferent(value: String) =
+    value.forall(v => value.count(_ == v) == 1)
+
+  private def resolve(input: String, distinctCharacters: Int): Int =
+    input
+      .sliding(distinctCharacters)
+      .zipWithIndex
+      .map { case (value, index) => (value, index + distinctCharacters) }
+      .find { case (value, _) => allDifferent(value) }
+      .get
+      ._2
+
+  override def part1(input: String): Int =
+    resolve(input, 4)
+
+  override def part2(input: String): Int =
+    resolve(input, 14)
+}
---
+++ src/test/scala/com/oodic/aoc/y2022/Day06Test.scala
@@ -0,0 +1,32 @@
+package com.oodic.aoc.y2022
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Day06Test extends AnyFlatSpec with Matchers {
+  private val testInput1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+  private val testInput2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+  private val testInput3 = "nppdvjthqldpwncqszvftbrmjlhg"
+  private val testInput4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+  private val testInput5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+
+  "Day06 - 2022" should "solve first part" in {
+    Day06.part1(testInput1) should equal(7)
+    Day06.part1(testInput2) should equal(5)
+    Day06.part1(testInput3) should equal(6)
+    Day06.part1(testInput4) should equal(10)
+    Day06.part1(testInput5) should equal(11)
+
+    Day06.part1(Day06.input) should equal(1566)
+  }
+
+  it should "solve second part" in {
+    Day06.part2(testInput1) should equal(19)
+    Day06.part2(testInput2) should equal(23)
+    Day06.part2(testInput3) should equal(23)
+    Day06.part2(testInput4) should equal(29)
+    Day06.part2(testInput5) should equal(26)
+
+    Day06.part2(Day06.input) should equal(2265)
+  }
+}
